3b03623e7657a17e7c77d9ff5a95a80fdede529e
Jaraffe <ccm1333@naver.com>
2022-07-03 21:08:50 +0900

    slot의 활성화 상태에 따라 Bag의 위치 및 사이즈가 확장 및 축소 되도록 추가.

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,60 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+[ExecuteInEditMode] // 에디터 테스트용.
 public class Inventory : MonoBehaviour
 {
-    [SerializeField]
-    private Transform slotParent;
+    public int columnMaxCount = 8;
+    public Vector2 slotSize = new Vector2(50, 50);
 
-    [SerializeField]
-    private Slot itemSlot;
-
-    [SerializeField]
-    private List<Item> itemDatas;
-
-    private List<Slot> itemSlots = new List<Slot>();
+    public RectTransform bag;
+    public RectTransform slotsParent;
+    private Slot[] itemSlots;
 
     private void OnValidate()
     {
-        //slots = slotParent.GetComponentsInChildren<Slot>();
+        // 처음 셋팅된 slot들이 해당 인벤토리의 최대 크기.
+        itemSlots = slotsParent.GetComponentsInChildren<Slot>(true);
     }
 
-    void Awake()
+    private void Update()
     {
-        foreach (var item in itemDatas)
+        // 최소, 최대값을 구해서 bag의 위치와 크기를 활성화되 slot에 맞춘다.
+        Vector2 min = new Vector2(float.MaxValue, float.MaxValue);
+        Vector2 max = new Vector2(-float.MaxValue, -float.MaxValue);
+
+        for (int i = 0; i < itemSlots.Length; ++i)
         {
-            Slot newSlot = Instantiate(itemSlot, slotParent);
-            newSlot.item = item;
-            itemSlots.Add(newSlot);
+            float x = (i % columnMaxCount) * slotSize.x;
+            float y = (i / columnMaxCount) * slotSize.y;
+
+            // 슬롯들을 배치한다.
+            var rt = itemSlots[i].GetComponent<RectTransform>();
+            rt.sizeDelta = slotSize;
+            rt.anchoredPosition = new Vector2(x, -y);
+
+            // 활성화 되어 있는 경우만, bag의 위치, 크기에 영향을 준다.
+            if (itemSlots[i].isActiveAndEnabled)
+            {
+                min.x = Mathf.Min(min.x, x);
+                min.y = Mathf.Min(min.y, y);
+
+                max.x = Mathf.Max(max.x, x + slotSize.x);
+                max.y = Mathf.Max(max.y, y + slotSize.y);
+            }
         }
 
-        FreshSlot();
+        // slot들이 모여있는 slots의 위치를 기준으로 bag의 위치, 크기를 조절한다.
+        // 현재는 bag, slots가 left top 일때만 워킹함.
+        bag.localPosition = slotsParent.localPosition;
+        bag.anchoredPosition += new Vector2(min.x, -min.y);
+        bag.sizeDelta = max - min;
     }
 
-    public void FreshSlot()
-    {
-        //int i = 0;
-        //for (; i < items.Count && i < slots.Length; ++i)
-        //{
-        //    slots[i].item = items[i];
-        //}
-        //for (; i < slots.Length; ++i)
-        //{
-        //    slots[i].item = null;
-        //}
-    }
-
-    public void AddItem(Item _item)
-    {
-        //if (items.Count < slots.Length)
-        //{
-        //    items.Add(_item);
-        //    FreshSlot();
-        //}
-        //else
-        //{
-        //    print("슬롯이 가득 차 있습니다.");
-        //}
-    }
 }
--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -5,24 +5,4 @@ using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
-    [SerializeField] Image image;
-
-    private Item _item;
-    public Item item
-    {
-        get { return _item; }
-        set 
-        { 
-            _item = value;
-            if (_item != null)
-            {
-                image.sprite = _item.itemImage;
-                image.color = new Color(1, 1, 1, 1);
-            }
-            else
-            {
-                image.color = new Color(1, 1, 1, 0);
-            }
-        }
-    }
 }
